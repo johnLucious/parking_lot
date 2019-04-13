231088398d81b3807332fdb4b190031710820ec8
Jansen <=>
2019-04-13 20:08:05 +0700

    Add class for Command

--- src/Command.java
+++ src/Command.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+public class Command extends Main {
+    private String[] args;
+    private int argc;
 
-public class Command {
+    public Command(String[] args, int argc){
+        this.args = args;
+        this.argc = argc;
+    }
+
+    public String getCommand(){
+        return this.args[0];
+    }
+
+    public String[] getArgs(){
+        return this.args;
+    }
 }
